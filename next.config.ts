64600ce62cc11295499ac0e10cc93163064a9b3e
Nitish Naveen <nitishnaveen5679@gmail.com>
2025-11-04 17:58:25 +0000

    use this image: https://www.compu2nd.in/blog/big_img/5947bdcfb309b00e8a6

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.compu2nd.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
