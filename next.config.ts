7eb2ea8bc4b79cd7d9518299b2b39b7bab30723c
Nitish Naveen <nitishnaveen5679@gmail.com>
2025-11-04 18:16:48 +0000

    in Explore RANDOM FOREST page: https://st.depositphotos.com/1006318/3172

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -49,6 +49,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'i0.wp.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'st.depositphotos.com',
         port: '',
         pathname: '/**',
       }
